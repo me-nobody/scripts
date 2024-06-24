8a935b1e71ea22988e07d19bc2ae6ba697371561
me-nobody <anubratadas@gmail.com>
2024-06-24 13:07:44 +0100

    accurate classifier obtained

--- train_adaBoost_classify_nuclei_230624_update.py
+++ train_adaBoost_classify_nuclei_230624_update.py
@@ -26,20 +26,20 @@ logger = logging.getLogger(__name__)
 
 scaler = MinMaxScaler()
 
-IN ="/users/ad394h/Documents/nuclei_segment/data/"
 
-OUT = "/users/ad394h/Documents/nuclei_segment/data/"
+DATA_PATH ="/users/ad394h/Documents/nuclei_segment/data/training_data/"
 
-DATA_PATH ="/users/ad394h/Documents/nuclei_segment/data/"
+MODEL_PATH ="/users/ad394h/Documents/nuclei_segment/models/"
 
-FEATURE_PATH = "/users/ad394h/Documents/nuclei_segment/data/val_he_images_features"
+
+FEATURE_PATH = "/users/ad394h/Documents/nuclei_segment/data/val_he_images_features/"
 
 LABEL_PATH = "/users/ad394h/Documents/nuclei_segment/data/val_he_image_labels/"
 
 
 input_csv = "nuclei_features.csv"
 
-training_data = "Final_GFP_train_df.csv"
+training_data = "final_GFP_train_df.csv"
 
 scaler = MinMaxScaler()
 
@@ -69,17 +69,25 @@ def prepare_data(training_data):
     xtest_df = pd.DataFrame(X_test)
     xtrain_df.columns = input_cols
     xtest_df.columns = input_cols
-    # xtrain_df.to_csv(os.path.join(DATA_PATH,"X_train.csv"),index=False)
-    # xtest_df.to_csv(os.path.join(DATA_PATH,"X_test.csv"),index=False)
+    xtest_df['target'] = test_targets
+    xtrain_df.to_csv(os.path.join(DATA_PATH,"X_train_{}.csv".format(timestr)),index=False)
+    xtest_df.to_csv(os.path.join(DATA_PATH,"X_test_{}.csv".format(timestr)),index=False)
+    logger.info(f"train data shape {xtrain_df.shape[0]},{xtrain_df.shape[1]}")
+    logger.info(f"test data shape {xtest_df.shape[0]},{xtest_df.shape[1]}")
     return X_train, X_test,train_targets,test_targets,scaler
     
 def train_data(*vars):
     X_train, X_test,train_targets,test_targets = vars
     from sklearn.ensemble import AdaBoostClassifier
     model = AdaBoostClassifier(n_estimators=100, algorithm="SAMME", random_state=0)
+    model_path = os.path.join(MODEL_PATH,"adaboost_classify_nuclei_pretrained.joblib")
+    with open(model_path,"wb") as f_pre:
+        joblib.dump(model,f_pre,protocol=5)
     model.fit(X_train, train_targets)
     # save the model
-    joblib.dump(model,os.path.join(MODEL_PATH,"adaboost_tumor.joblib"))
+    model_path = os.path.join(MODEL_PATH,"adaboost_classify_nuclei.joblib")
+    with open(model_path,"wb") as f:
+        joblib.dump(model,f,protocol=5)
     # get the scores
     # accuracy
     train_accuracy = model.score(X_train, train_targets)
@@ -104,6 +112,24 @@ def read_label(LBL_IMG):
     labels = io.imread(img_file)
     return labels
 
+# function to pair features df and label image
+def create_feature_label_pair(FEATURE_PATH,LABEL_PATH):
+    feature_file_list = os.listdir(FEATURE_PATH)
+    label_file_list = os.listdir(LABEL_PATH)
+    logger.info(f"number of feature files {len(feature_file_list)}")
+    logger.info(f"number of label files {len(label_file_list)}")
+    assert len(feature_file_list)>0,"no files present"
+    assert len(label_file_list)>0,"no files present"
+    if all(feature[-4:] == ".csv" for feature in feature_file_list):
+        logger.info("all csv files in folder")
+    if all(img[-4:] == ".png" for img in label_file_list):
+        logger.info("all png files in folder")
+    # matched image and label
+    feature_label_pair_list = [(a,b) for a in feature_file_list for b in label_file_list if a[:21] == b[:21]]
+    logger.info(f"{len(feature_label_pair_list)} feature_label_pairs_created")    
+    return feature_label_pair_list    
+
+
 def predict_class(input_csv=None,scaler=None,model=None):
     # predict class has to identify which image it is predicting for
     logger.info("predicting class")
@@ -126,6 +152,24 @@ def predict_class(input_csv=None,scaler=None,model=None):
     logger.info(f"columns are {test_classes.columns}")
     return test_class_df
 
+def predict_with_saved_model(X_test,test_targets,scaler):
+    # predict class has to identify which image it is predicting for
+    logger.info("predicting class")
+    # get the features list
+    saved_model_path = os.path.join(MODEL_PATH,"adaboost_classify_nuclei.joblib")
+    with open(saved_model_path, "rb") as f_model:
+        clf = joblib.load(f_model)      
+    # accuracy
+    test_accuracy = clf.score(X_test, test_targets)
+    # F-score
+    predicted_test_targets = clf.predict(X_test)
+    test_f1 = f1_score(test_targets,predicted_test_targets, average='macro')
+    
+    logger.info(f"test accuracy with saved model {test_accuracy}")
+    logger.info(f"test F1 score with saved model {test_f1}")
+
+
+
 def cell_types(test_class_df):
     class_dict = {0:'normal',1:'tumor'}
     test_class_df['class'] = test_class_df['class'].map(class_dict)
@@ -136,17 +180,6 @@ def cell_types(test_class_df):
             percent_tumor = (int(a[1])/test_class_df.shape[0])*100
             logger.info(f"percentage of tumor cells {percent_tumor}")    
 
-# function to pair features df and label image
-def class_df_label_image_pair(class_df,label_image):    
-    logger.info("reading file")
-    img_file = os.path.join(IN,LBL_IMG)
-    labels = io.imread(img_file)
-    image_name = LBL_IMG[:-4]
-    return labels
-    for df in class_df:
-        for image in label_image:
-            if df == image:
-                return df,image            
 
 
 def relabel_image(class_df,label_image):
@@ -171,46 +204,39 @@ def relabel_image(class_df,label_image):
     new_labels =flat_new_labels.reshape(label_image.shape)   
     return new_labels
 
-def create_feature_label_pair(FEATURE_PATH,LABEL_PATH):
-    feature_file_list = os.listdir(FEATURE_PATH)
-    label_file_list = os.listdir(LABEL_PATH)
-    logger.info(f"number of feature files {len(feature_file_list)}")
-    logger.info(f"number of label files {len(label_file_list)}")
-    assert len(feature_file_list)>0,"no files present"
-    assert len(label_file_list)>0,"no files present"
-    if all(feature[-4:] == ".csv" for feature in feature_file_list):
-        logger.info("all csv files in folder")
-    if all(img[-4:] == ".png" for img in label_file_list):
-        logger.info("all png files in folder")
-    # matched image and label
-    feature_label_pair_list = [(a,b) for a in feature_file_list for b in label_file_list if a[:21] == b[:21]]
-    logger.info(f"{len(feature_label_pair_list)} feature_label_pairs_created")    
-    return feature_label_pair_list    
 
 
 if __name__ == "__main__":
     logger.info("start train classifier and classify nuclei")
     logger.info("preparing data ....")
-    create_feature_label_pair(FEATURE_PATH,LABEL_PATH)
+    # get the feature label pair list
+    feature_label_pair_list = create_feature_label_pair(FEATURE_PATH,LABEL_PATH)
     # X_train, X_test,train_targets,test_targets,scaler = prepare_data(training_data=training_data)    
-    # logger.info("training data ...")
+    logger.info("training data ...")
     # model = train_data(X_train, X_test,train_targets,test_targets)
-    # try:
-    #     os.path.join(OUT,LBL_IMG)
-    #     logger.info(f"{LBL_IMG} image file recongnized")
-    # except AssertionError as err:
-    #     logger.info(f"label image not found")
-    # label_image = read_label(LBL_IMG)
-    # assert input_csv is not None,logger.info(f"input csv not found")
-    # test_class_df = predict_class(input_csv=input_csv,scaler=scaler,model=model)
-    # new_label_image = relabel_image(test_class_df,label_image)
-    # assert new_label_image is not None,logger.info(f"relablled image missing")    
-    # io.imsave(fname=os.path.join(OUT,f"{IMG[:-4]}_classified_image.png"),arr=new_label_image)
-    # try:
-    #     isinstance(test_class_df,pd.DataFrame)
-    #     logger.info(f"test dataframe detected")
-    # except AssertionError as err:
-    #     logger.info(f"test dataframe not found")    
-    # cell_types(test_class_df)
+    try:
+        model_path = os.path.join(MODEL_PATH,"adaboost_classify_nuclei.joblib")
+        with open(model_path, "rb") as f_model:
+            model = joblib.load(f_model)
+    except FileNotFoundError:
+        print("model file not found")
+
+    if os.path.exists(model_path) and isinstance(model_path,joblib):
+        logger.info(f"adaboost classifier obtained")
+    else:
+        logger.info("please check model is the saved model")    
+    # predict_with_saved_model(X_test,test_targets,scaler)   
+     
+     
+    test_class_df = predict_class(input_csv=input_csv,scaler=scaler,model=model)
+    new_label_image = relabel_image(test_class_df,label_image)
+    assert new_label_image is not None,logger.info(f"relabelled image missing")    
+    io.imsave(fname=os.path.join(OUT,f"{IMG[:-4]}_classified_image.png"),arr=new_label_image)
+    try:
+        isinstance(test_class_df,pd.DataFrame)
+        logger.info(f"test dataframe detected")
+    except AssertionError as err:
+        logger.info(f"test dataframe not found")    
+    cell_types(test_class_df)
     
     
